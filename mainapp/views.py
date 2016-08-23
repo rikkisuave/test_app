e07cc477257deb9be7712ac006abc5196da3eb7d
rikkisuave <dark_bwoy@hotmail.com>
2016-08-24 00:31:03 +0100

    second test

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,9 @@
+from django.template.loader import get_template
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("*******TEST SITE**********")
+    #template=get_template('boottemp.html')
+    return render(request,'boottemp2.html')
--- test_app/settings.py
+++ test_app/settings.py
@@ -118,7 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-
+STATIC_ROOT= ''
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static/'),
+)
+
 
